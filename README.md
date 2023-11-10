7da61589517e155460fa1f28c7b967eb0460eced
startbit96 <timschwarzbrunn2@googlemail.com>
2023-11-10 22:54:40 +0100

    added termion support

--- README.md
+++ README.md
@@ -9,4 +9,7 @@ Maze-generation and maze-solving written in Rust.
 ### Implemented maze-solving algorithms.
 
 ## Notes.
+
+In order to make the visualization of the mazes more appealing, change the size of the cells in your terminal so they are square. In kitty you can change this using the [modify_font setting](https://sw.kovidgoyal.net/kitty/conf/#opt-kitty.modify_font).
+
 For understanding and implementing the algorithms, the Python API [mazelib](https://github.com/john-science/mazelib/tree/main) was very helpful. If you are interested how the algorithms work, see the linked github-repository for the provided documentation.
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use rand::seq::SliceRandom;
 use rand::thread_rng;
 
+extern crate termion;
+
 const SYMBOL_MAZE_BORDER_CORNER_TOP_LEFT: char = '\u{2554}';
 const SYMBOL_MAZE_BORDER_CORNER_TOP_RIGHT: char = '\u{2557}';
 const SYMBOL_MAZE_BORDER_CORNER_BOTTOM_LEFT: char = '\u{255A}';
@@ -11,6 +13,11 @@ const SYMBOL_MAZE_BORDER_LEFT: char = '\u{2551}';
 const SYMBOL_MAZE_BORDER_RIGHT: char = '\u{2551}';
 const SYMBOL_MAZE_FIELD_ACCESSIBLE: char = ' ';
 const SYMBOL_MAZE_FIELD_BLOCKED: char = '\u{2588}';
+
+const TERMION_OFFSET_X: u16 = 1;
+const TERMION_OFFSET_Y: u16 = 1;
+const BORDER_WIDTH_X: u16 = 11;
+const BORDER_WIDTH_Y: u16 = 3;
 
 #[derive(Debug, Default)]
 enum MazeGenerationAlgorithm {
@@ -37,14 +44,45 @@ impl Maze {
     }
 
     fn draw(&self) {
-        // Draw the maze and also add a border around it.
+        // Top border.
+        print!(
+            "{}{}",
+            termion::clear::All,
+            termion::cursor::Goto(TERMION_OFFSET_X, TERMION_OFFSET_Y)
+        );
         print!("{}", SYMBOL_MAZE_BORDER_CORNER_TOP_LEFT);
-        for _ in &self.data[0] {
+        for _ in 1..=(self.width + 2 * (BORDER_WIDTH_X as usize - 1)) {
             print!("{}", SYMBOL_MAZE_BORDER_TOP);
         }
         println!("{}", SYMBOL_MAZE_BORDER_CORNER_TOP_RIGHT);
-        for row in &self.data {
-            print!("{}", SYMBOL_MAZE_BORDER_LEFT);
+
+        // Extra space between the maze and the top border (if wanted).
+        for extra_space in 1..BORDER_WIDTH_Y {
+            println!(
+                "{}{}{}",
+                SYMBOL_MAZE_BORDER_LEFT,
+                termion::cursor::Goto(
+                    TERMION_OFFSET_X + 2 * BORDER_WIDTH_X + self.width as u16 - 1,
+                    TERMION_OFFSET_Y + extra_space
+                ),
+                SYMBOL_MAZE_BORDER_RIGHT,
+            );
+        }
+
+        // Maze together with the border on the left and right.
+        for (idx_row, row) in self.data.iter().enumerate() {
+            print!(
+                "{}{}{}",
+                termion::cursor::Goto(
+                    TERMION_OFFSET_X,
+                    TERMION_OFFSET_Y + BORDER_WIDTH_Y + idx_row as u16
+                ),
+                SYMBOL_MAZE_BORDER_LEFT,
+                termion::cursor::Goto(
+                    TERMION_OFFSET_X + BORDER_WIDTH_X,
+                    TERMION_OFFSET_Y + BORDER_WIDTH_Y + idx_row as u16
+                )
+            );
             for &bit in row {
                 print!(
                     "{}",
@@ -55,10 +93,32 @@ impl Maze {
                     }
                 );
             }
-            println!("{}", SYMBOL_MAZE_BORDER_RIGHT);
+            println!(
+                "{}{}",
+                termion::cursor::Goto(
+                    TERMION_OFFSET_X + 2 * BORDER_WIDTH_X + self.width as u16 - 1,
+                    TERMION_OFFSET_Y + BORDER_WIDTH_Y + idx_row as u16
+                ),
+                SYMBOL_MAZE_BORDER_RIGHT
+            );
         }
+
+        // Extra space between the maze and the bottom border (if wanted).
+        for extra_space in 1..BORDER_WIDTH_Y {
+            println!(
+                "{}{}{}",
+                SYMBOL_MAZE_BORDER_LEFT,
+                termion::cursor::Goto(
+                    TERMION_OFFSET_X + 2 * BORDER_WIDTH_X + self.width as u16 - 1,
+                    TERMION_OFFSET_Y + BORDER_WIDTH_Y + self.height as u16 + extra_space - 1
+                ),
+                SYMBOL_MAZE_BORDER_RIGHT,
+            );
+        }
+
+        // Bottom border.
         print!("{}", SYMBOL_MAZE_BORDER_CORNER_BOTTOM_LEFT);
-        for _ in &self.data[0] {
+        for _ in 1..=(self.width + 2 * (BORDER_WIDTH_X as usize - 1)) {
             print!("{}", SYMBOL_MAZE_BORDER_BOTTOM);
         }
         println!("{}", SYMBOL_MAZE_BORDER_CORNER_BOTTOM_RIGHT);
@@ -170,7 +230,7 @@ impl Maze {
 }
 
 fn main() {
-    let mut maze = Maze::new(81, 81);
+    let mut maze = Maze::new(41, 21);
     maze.generate_maze(MazeGenerationAlgorithm::default());
     maze.draw();
 }
